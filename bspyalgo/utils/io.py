345f8372874f1cd78f815ff99cf8a576dce77a1e
ualegre <u.alegre@utwente.nl>
2020-08-03 13:55:49 +0200

    Migrated from json to yaml

--- bspyalgo/utils/io.py
+++ bspyalgo/utils/io.py
@@ -8,7 +8,8 @@ import codecs
 import pickle
 import shutil
 import torch
-
+import yaml
+import io
 
 import numpy as np
 
@@ -53,18 +54,26 @@ def save_torch(torch_model, file_path):
     torch.save(state_dic, file_path)
 
 
-def load_configs(file):
-    object_text = codecs.open(file, 'r', encoding='utf-8').read()
-    return json.loads(object_text)
+# def load_configs(file):
+#     object_text = codecs.open(file, 'r', encoding='utf-8').read()
+#     return json.loads(object_text)
 
 
-def save_configs(configs, file):
-    with open(file, 'w') as f:
-        for key in configs:
-            if type(configs[key]) is np.ndarray:
-                configs[key] = configs[key].tolist()
-        json.dump(configs, f, indent=4)
-        f.close()
+def load_configs(file_name):
+    with open(file_name) as f:
+        return yaml.load(f, Loader=IncludeLoader)
+
+
+# def save_configs(configs, file):
+#     with open(file, 'w') as f:
+#         for key in configs:
+#             if type(configs[key]) is np.ndarray:
+#                 configs[key] = configs[key].tolist()
+#         json.dump(configs, f, indent=4)
+#         f.close()
+def save_configs(configs, file_name):
+    with open(file_name, 'w') as f:
+        yaml.dump(configs, f, default_flow_style=False)
 
 
 def create_directory(path, overwrite=False):
@@ -84,6 +93,59 @@ def create_directory_timestamp(path, name, overwrite=False):
     datetime = time.strftime("%Y_%m_%d_%H%M%S")
     path = os.path.join(path, name + '_' + datetime)
     return create_directory(path, overwrite=overwrite)
+
+
+class IncludeLoader(yaml.Loader):
+    """
+    yaml.Loader subclass handles "!include path/to/foo.yml" directives in config
+    files.  When constructed with a file object, the root path for includes
+    defaults to the directory containing the file, otherwise to the current
+    working directory. In either case, the root path can be overridden by the
+    `root` keyword argument.
+
+    When an included file F contain its own !include directive, the path is
+    relative to F's location.
+
+    Example:
+        YAML file /home/frodo/one-ring.yml:
+            ---
+            Name: The One Ring
+            Specials:
+                - resize-to-wearer
+            Effects:
+                - !include path/to/invisibility.yml
+
+        YAML file /home/frodo/path/to/invisibility.yml:
+            ---
+            Name: invisibility
+            Message: Suddenly you disappear!
+
+        Loading:
+            data = IncludeLoader(open('/home/frodo/one-ring.yml', 'r')).get_data()
+
+        Result:
+            {'Effects': [{'Message': 'Suddenly you disappear!', 'Name':
+                'invisibility'}], 'Name': 'The One Ring', 'Specials':
+                ['resize-to-wearer']}
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(IncludeLoader, self).__init__(*args, **kwargs)
+        self.add_constructor('!include', self._include)
+        if 'root' in kwargs:
+            self.root = kwargs['root']
+        elif isinstance(self.stream, io.TextIOWrapper):
+            self.root = os.path.dirname(self.stream.name)
+        else:
+            self.root = os.path.curdir
+
+    def _include(self, loader, node):
+        oldRoot = self.root
+        filename = os.path.join(self.root, loader.construct_scalar(node))
+        self.root = os.path.dirname(filename)
+        self.root = oldRoot
+        with open(filename, 'r') as f:
+            return yaml.load(f)
 
 
 if __name__ == '__main__':
